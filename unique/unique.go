c84f3447e9daedeb0880ad30eebd04717da4db64
S. Kvartnikov <s.kvartnikov@butik.ru>
2018-12-06 18:22:07 +0300

    Added unique ints method

--- unique/unique.go
+++ unique/unique.go
@@ -15,3 +15,19 @@ func Strings(input []string) []string {
 
 	return u
 }
+
+// Ints returns a unique slice of input ints
+// It not changes the order of input
+func Ints(input []int) []int {
+	u := make([]int, 0, len(input))
+	m := make(map[int]bool)
+
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+
+	return u
+}
